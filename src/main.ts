aa60c08bdce6fdda803ce77586629ae365a18b17
Yorman <yorsh355@gmail.com>
2025-04-01 17:29:28 -0500

    Agregar configuración de entorno y middleware de registro en el servidor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(morgan('dev'));
+
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('api/v1');
+
+  console.log(configService.get('PORT'));
+
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
