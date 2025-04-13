20ed1a6daa0d8dfc5c5821111ade9710ae82fd7d
Yorman <yorsh355@gmail.com>
2025-04-12 21:10:41 -0500

    Añadir validación global con ValidationPipe y ajustar configuración en main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
 import { CORS } from './constants/cors';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(morgan('dev'));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   const configService = app.get(ConfigService);
 
