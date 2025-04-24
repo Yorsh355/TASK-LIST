ae314dcbb9c4ed8dfcdd647b70557311055c33ae
Yorman <yorsh355@gmail.com>
2025-04-24 10:50:57 -0500

    Refactor findAllTask para utilizar ExpressRequest y mejorar la claridad del código; eliminar autenticación comentada en createTask.

--- src/tasks/controllers/tasks.controller.ts
+++ src/tasks/controllers/tasks.controller.ts
@@ -10,6 +10,7 @@ import {
   Query,
   Request,
 } from '@nestjs/common';
+import { Request as ExpressRequest } from 'express';
 import { TasksService } from '../services/tasks.service';
 import { CreateTaskDto } from '../dto/create-task.dto';
 import { UpdateTaskDto } from '../dto/update-task.dto';
@@ -17,24 +18,25 @@ import { Auth } from '../../auth/decorators/auth.decorator';
 import { ValidRoles } from 'src/auth/interfaces/valid-roles';
 import { TaskEntity } from '../entities/task.entity';
 import { QueryParamsDto } from '../dto/query-params.dto';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 @Controller('tasks')
+@Auth()
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
-  @Auth(ValidRoles.ADMIN, ValidRoles.USER)
   createTask(@Body() createTaskDto: CreateTaskDto) {
     return this.tasksService.createTask(createTaskDto);
   }
 
   @Get()
-  @Auth(ValidRoles.ADMIN, ValidRoles.USER)
   findAllTask(
-    @Request() req,
+    @Request() req: ExpressRequest,
     @Query() queryParamsDto: QueryParamsDto,
   ): Promise<TaskEntity[]> {
-    return this.tasksService.findAllTask(req.user, queryParamsDto);
+    const user = req.user as UserEntity;
+    return this.tasksService.findAllTask(user, queryParamsDto);
   }
 
   @Get(':id')
